58e07fc5300908f3fb8d81a95b4da84a92aa19e6
Sean D'Souza <sdammobubbles@gmail.com>
2021-06-05 14:40:42 -0400

    Create CLI program for emulator

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,27 @@
-mod instruction;
+mod riscv;
+use std::env;
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
 
 fn main() {
-    instruction::decode(0x234234);
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        panic!(" Usage: rv64_emulator <filename>");
+    }
+    let path = Path::new(&args[1]);
+    let display = path.display();
+
+    let mut file = match File::open(path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+    let mut encoded_instructions = Vec::new();
+    match file.read_to_end(&mut encoded_instructions) {
+        Err(why) => panic!("couldn't read {}: {}", display, why),
+        Ok(_) => println!("{} read", display),
+    };
+
+    let mut cpu = riscv::cpu::Cpu::new(encoded_instructions);
+    cpu.run();
 }
